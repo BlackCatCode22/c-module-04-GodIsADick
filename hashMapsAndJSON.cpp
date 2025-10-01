afaed218dd9a0dd4e62c756fd4e9027b5ca55a78
GodIsADick <bwilson0908@yahoo.com>
2025-10-01 11:47:11 -0700

    Add files via upload

---
+++ hashMapsAndJSON.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <C:/Users/bwils/CLionProjects/Beginner Projects/hashMapsAndJSON/include/json.hpp.txt>
+#include <string>
+#include <unordered_map>
+using json = nlohmann::json;
+using namespace std;
+
+int main() {
+    cout << "\nWelcome to JSON!\n" << endl;
+
+    json myStuList = {
+        {"Arturo", 1234},
+        {"Blake", 5432},
+        {"Chase", 4321}
+    };
+
+    // Access the values like a hash map
+    cout << "Arturo's Student ID: " << myStuList["Arturo"] << " (this is the value of the key/value pair)" << endl;
+    cout << "Blake's Student ID: " << myStuList["Blake"] << endl;
+    cout << "Chase's Student ID: " << myStuList["Chase"] << endl;
+
+    /* Create a C++ hash map that uses three student names as keys and int values for
+     * their test scores - name(string) : score(int) */
+    unordered_map<string, int>myStuTestScores;
+
+    myStuTestScores["Arturo"] = 95;
+    myStuTestScores["Blake"] = 100;
+    myStuTestScores["Chase"] = 87;
+
+    cout << "\nArturo's test score is: " << myStuTestScores["Arturo"] << endl;
+    cout << "Blake's test score is: " << myStuTestScores["Blake"] << endl;
+    cout << "Chase's test score is: " << myStuTestScores["Chase"] << endl;
+
+
+    return 0;
+}
